205d5fc98290f9fd49f848ea5027ef6c13db4cd4
Abzaek <abzaek@hotmail.com>
2024-08-04 18:58:23 +0300

    structurized the module

---
+++ main/main.go
@@ -0,0 +1,13 @@
+package main
+
+// import (
+// 	"fmt"
+
+// 	Str "github.com/Abzaek/go_/Strings"
+// 	"github.com/Abzaek/go_/GradeCalculator"
+// )
+
+func main() {
+	// gradeCalc.StartProgram()
+	// fmt.Println(Str.IsPalindrome())
+}
